287853b8c6ea64c1ff38a488cf49af9b13ae5c71
354malek75 <maleklabib426@gmail.com>
2026-06-01 20:22:35 +0300

    Refactor build.gradle.kts by removing unused sections
    Removed buildscript section and pre-commit git hook task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,12 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-    }
-    dependencies {
-        classpath("com.diffplug.spotless:spotless-plugin-gradle:6.19.0")
-    }
-}
-
 plugins {
     id("com.android.application") version "8.0.2" apply false
     id("com.android.library") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
-    id("com.diffplug.spotless") version "6.19.0" apply false
-
 }
 
-subprojects {
-    afterEvaluate {
-        project.apply("../spotless.gradle")
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
-}
-
-task("addPreCommitGitHookOnBuild") {
-    println("⚈ ⚈ ⚈ Running Add Pre Commit Git Hook Script on Build ⚈ ⚈ ⚈")
-    exec {
-        commandLine("cp", "./.scripts/pre-commit", "./.git/hooks")
-    }
-    println("✅ Added Pre Commit Git Hook Script.")
 }
